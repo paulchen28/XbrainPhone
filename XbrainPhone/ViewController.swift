76f8ea1a46ce269af2580d6eea21df4d2e9b26a7
paulchen1979 <paul428728@gmail.com>
2021-12-08 02:25:29 +0800

    Initial Commit

---
+++ XbrainPhone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XbrainPhone
+//
+//  Created by 陳家騏 on 2021/12/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
